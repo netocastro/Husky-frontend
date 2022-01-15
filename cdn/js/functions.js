dcdd0e3fe6c2017da9e694fca1f08a4dbbcda7ca
graveeler2 <57375301+netocastro@users.noreply.github.com>
2022-01-15 02:56:52 -0300

    terminado
    Todas as requisições estão funcionando. falta melhor o front end

--- cdn/js/functions.js
+++ cdn/js/functions.js
@@ -1,12 +1,12 @@
 function validateFields(data, dadosForm){
 
-      $(dadosForm.find('input, select, textarea')).each(function(index) {
+      /*$(dadosForm.find('input, select, textarea')).each(function(index) {
             $(`${$(this).prop('tagName')}[name=${$(this).attr('name')}]`).removeClass('is-invalid');
             $(`#error-${$(this).attr('name')}`).fadeOut().remove();
-      });
+      });*/
       $('#success').fadeOut().remove();
 
-      if(data.emptyFields){
+     /* if(data.emptyFields){
             data.emptyFields.forEach(element => {	
                   $(`[name=${element}]`).addClass('is-invalid');
                   $(`[name=${element}]`).after(`<div id='error-${element}' class='text-danger'>Campo vazio</div>`);
@@ -20,20 +20,11 @@ function validateFields(data, dadosForm){
                   $(`[name=${field}]`).addClass('is-invalid');
                   $(`[name=${field}]`).after(`<div id='error-${field}' class='text-danger'>${fields[field]}</div>`)
             }
-      }
+      }*/
 
       if(data.success){
-            $('button[type=submit]').after(`<h6 id="success" class="bg-success text-light p-2 mt-3 rounded text-center">${data.success}</h6>`).hide().fadeIn();
+            $('button[type=submit]').after(`<h6 id="success" class="bg-success text-light p-2 rounded text-center">${data.success}</h6>`).hide().fadeIn();
             $('.form-control').val('');
-      }
-}
-
-function show_image(input) {
-      if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                  $('#preview').attr('src', e.target.result);
-            }
-            reader.readAsDataURL(input.files[0]);
+            $('.form-select').val('');
       }
 }
